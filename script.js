8f0c04dc8b4d59a8e431e841b7c586fdc94dda40
Asikorski11 <97486169+Asikorski11@users.noreply.github.com>
2022-01-14 02:00:16 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+let gamer = prompt("Welcome to GC Mini Golf! What is your $(name)?");
+console.log(gamer);
+let game = prompt("Hi, ${gamer}! Would you like to play 3 or 6 holes?");
+let par = par * game;
+for (let index = 0; i < game; index++) {
+    let putts = 0;
+    putts = prompt("How many putts for hole ${i + 1}?) (par: 3)");
+    let par = 3;
+    score += Number(putts);
+}
+
+if (score === par) {
+    alert("Great job, ${gamer}. Your total par was : 0.")
+    else if (score > par) }
+    alert("Nice try, ${gamer}. Your total par was: +${score - par}")
+    else (score < par) }
+    alert(Good game, ${gamer}. Your total par was: -${par - score}")
+    }
