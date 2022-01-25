378c375aa6091dcc89c23e7d2081d5df6b1f688f
Asikorski11 <97486169+Asikorski11@users.noreply.github.com>
2022-01-24 20:03:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
-let gamer = prompt("Welcome to GC Mini Golf! What is your $(name)?");
-console.log(gamer);
-let game = prompt("Hi, ${gamer}! Would you like to play 3 or 6 holes?");
-let par = par * game;
-for (let index = 0; i < game; index++) {
-    let putts = 0;
-    putts = prompt("How many putts for hole ${i + 1}?) (par: 3)");
-    let par = 3;
-    score += Number(putts);
+let par = 3;
+let gameNum = 0;
+let score = 0;
+
+let gamer = prompt("Welcome to GC Mini Golf! What is your name?");
+while (gameNum !== 3 && gameNum !== 6) {
+  gameNum = prompt("Hi, gamer! Would you like to play 3 or 6 holes?");
+  gameNum = Number(gameNum);
+}
+
+par = par * gameNum;
+
+for (i = 0; i < gameNum; i++) {
+	console.log(i);
+
+  let holeNum = i + 1;
+
+  let putts = prompt(`How many putts for hole ${holeNum}?`);
+  
+  score += Number(putts);
+  
+  console.log("holeNum", holeNum);
+  console.log("putts", putts);
+  console.log("score", "score");
 }
 
 if (score === par) {
-    alert("Great job, ${gamer}. Your total par was : 0.")
-    else if (score > par) }
-    alert("Nice try, ${gamer}. Your total par was: +${score - par}")
-    else (score < par) }
-    alert(Good game, ${gamer}. Your total par was: -${par - score}")
-    }
+	alert("Great job, gamer. Your total par was: 0");
+} else if (score < par) {
+	alert(`Great job, gamer. Your total par was: -${par - score}`);
+} else {
+	alert(`Nice try, gamer. Your total par was: +${score - par}`);
+}
